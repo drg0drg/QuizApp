e3f2373409f0dac9d9b3709e0c2c18edff290ab1
drg0drg <dragomir_costin_dragos@yahoo.com>
2020-04-07 16:54:56 +0100

    Addressing issues with basic CSS. Advance the pseudocode. Create functional play button, timer, EventListener

--- script.js
+++ script.js
@@ -18,4 +18,51 @@
 // Add this function in the same addEventListener for the <play> button
 // Create compare function that compares the user input to the correct answer
 // Inside this function, create if/else loop -> if userIn===correctAnswer than increase score, else decrease time
+// Create function to stop the game
+// Inside this function, create if loop -> if timer=0 || no questions left than stop game
+
+
+
+
+// VARIABLES
+
+var timerValEl = document.getElementById("tm-val");
+var scoreValEl = document.getElementById("sc-val");
+var startBtn = document.getElementById("play");
+var stopBtn = document.getElementById("stop");
+var nextBtn = document.getElementById("next");
+var scoretBtn = document.getElementById("score");
+
+
+let timerValShown = 5;
+let scoreValShown = 0;
+var timerInterval;
+
+
+// this is only to be able to display the timerValShown when the page loads
+timerValEl.textContent = timerValShown;
+// this is only to be able to display the scoreValShown when the page loads
+scoreValEl.textContent = scoreValShown;
+
+
+
+
+
+// Adding the EventListener to start button to launch timer function
+// Inside EventListener is the function <timer> which uses the method setInterval
+// setInterval method has two parameters, (function, interval). The interval is in milliseconds, hence the second argument is <1000>
+startBtn.addEventListener("click", function timer(){
+    let timerVAL = setInterval(function (){
+        timerValEl.textContent = timerValShown;
+        // timerValShown--    -->>  if this is here, timer stops at 1 instead of 0;
+        if(timerValShown === 0) {
+            clearInterval(timerVAL);
+        }
+        timerValShown--;
+    }, 1000);
+});
+
+
+
+
 
