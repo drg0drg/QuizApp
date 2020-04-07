5e45dd9110c772f71c14eb654079c1eafe0d341a
drg0drg <dragomir_costin_dragos@yahoo.com>
2020-04-07 11:56:36 +0100

    Preliminary CSS responsive for mobile/tablet/desktop. Preliminary JS pseudocode started

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+// -------------------------------------------//
+// ------------PSEUDOCODE QUIZ APP------------//
+// -------------------------------------------//
+
+
+
+// Create sets of Q+A
+// These sets are objects with keys and values
+// Designate the correct answer. This will be compared later to the user input
+// These objects form an array of Q/A sets
+// Create timer countdown
+// Set the timer at a specific value
+// By an addEventListener, start the timer when <play> button is pressed
+// By an addEventListener, stop the timer when <stop> button is pressed
+// Create a function that runs through the QA array and display each set
+// Add this function in the same addEventListener for the <play> button
+// Create compare function that compares the user input to the correct answer
+// Inside this function, create if/else loop -> if userIn===correctAnswer than increase score, else decrease time
+
