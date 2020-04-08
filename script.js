c2e24329fb494b073360b9cac8847649566291e8
drg0drg <dragomir_costin_dragos@yahoo.com>
2020-04-08 08:50:55 +0100

    Functionality of the timer script and loading the first question addressed. Answers do not load

--- script.js
+++ script.js
@@ -27,41 +27,41 @@
 
 
 
-// let questions = [
-//     {
-//       question: "Inside which HTML element do we put the JavaScript??",
-//       choice1: "<script>",
-//       choice2: "<javascript>",
-//       choice3: "<js>",
-//       choice4: "<scripting>",
-//       answer: 1
-//     },
-//     {
-//       question:
-//         "What is the correct syntax for referring to an external script called 'xxx.js'?",
-//       choice1: "<script href='xxx.js'>",
-//       choice2: "<script name='xxx.js'>",
-//       choice3: "<script src='xxx.js'>",
-//       choice4: "<script file='xxx.js'>",
-//       answer: 3
-//     },
-//     {
-//       question: " When was C++ created by Bjarne Stroustrup, the Danish computer scientist?",
-//       choice1: "1878",
-//       choice2: "1973",
-//       choice3: "2015",
-//       choice4: "1983",
-//       answer: 4
-//     },
-//     {
-//         question: "What is <addEventListener> in JS ?",
-//         choice1: "a string",
-//         choice2: "a method", 
-//         choice3: "a variable", 
-//         choice4: "un pain au chocolat",
-//         answer: 2
-//     }
-//   ];
+let questions = [
+    {
+      question: "Inside which HTML element do we put the JavaScript??",
+      choice1: "<script>",
+      choice2: "<javascript>",
+      choice3: "<js>",
+      choice4: "<scripting>",
+      answer: 1
+    },
+    {
+      question:
+        "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      choice1: "<script href='xxx.js'>",
+      choice2: "<script name='xxx.js'>",
+      choice3: "<script src='xxx.js'>",
+      choice4: "<script file='xxx.js'>",
+      answer: 3
+    },
+    {
+      question: " When was C++ created by Bjarne Stroustrup, the Danish computer scientist?",
+      choice1: "1878",
+      choice2: "1973",
+      choice3: "2015",
+      choice4: "1983",
+      answer: 4
+    },
+    {
+        question: "What is <addEventListener> in JS ?",
+        choice1: "a string",
+        choice2: "a method", 
+        choice3: "a variable", 
+        choice4: "un pain au chocolat",
+        answer: 2
+    }
+  ];
 
 
 
@@ -116,7 +116,7 @@ startBtn.addEventListener("click", function timer(){
         }
         timerValShown--;
     }, 1000);
-    // loadQuestion();
+    loadQuestion();
 
 });
 
@@ -137,6 +137,7 @@ startBtn.addEventListener("click", function timer(){
 
 function loadQuestion (questionIndex){
     document.getElementById("play").disabled = true;
+    questionIndex=0;
     var localQuestion = questions[questionIndex];
     questionEl.textContent = localQuestion.question;
     option1.textContent = localQuestion.choice1;
@@ -145,3 +146,30 @@ function loadQuestion (questionIndex){
     option4.textContent = localQuestion.choice4;
 };
 
+
+
+
+
+
+// function loadNextQuestion(){
+//     var userChoice = document.querySelector('input[type=radio]:checked');
+//     if(!userChoice){
+//         alert("Please select answer.");
+//         return;
+//     }
+//     var answer = userChoice.value;
+//     if(questions[currentQuestion].answer === answer){
+//         score += 1;
+//     }
+//     userChoice.checked = false;
+//     currentQuestion++;
+//     if (currentQuestion == totquestions){
+//         startBtn.style.visibility='hidden';
+//         nextBtn.style.visibility='hidden';
+//         quizContainer.style.display="none";
+//         result.style.display="";
+//         result.textContent = score;
+//         return;
+//     }
+//     loadQuestion(currentQuestion);
+// }
